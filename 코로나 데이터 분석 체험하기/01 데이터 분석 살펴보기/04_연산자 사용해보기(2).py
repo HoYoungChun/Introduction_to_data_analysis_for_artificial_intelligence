f3c9279b834955ea5d9e14588cd56aca291fe811
HoYoungChun <ho723@naver.com>
2020-09-20 19:13:03 +0900

    Create 04_연산자 사용해보기(2).py

---
+++ 코로나 데이터 분석 체험하기/01 데이터 분석 살펴보기/04_연산자 사용해보기(2).py
@@ -0,0 +1,10 @@
+# 1. 30 곱하기 3의 결과를 구하고, result_1에 저장해봅시다.
+result_1 = 30*3
+
+# 2. 65 나누기 5의 결과를 구하고, result_2에 저장해봅시다.
+result_2 = 65/5
+
+# 3. result_1과 result_2의 값을 곱해서 result_3에 저장하고,
+# result_3의 결과를 출력해봅시다.
+result_3 = result_1 * result_2
+print(result_3)
